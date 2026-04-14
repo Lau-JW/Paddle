67408b42abb8776987bf134a85a54b17766f86fd
Lau-JW <3110263305@qq.com>
2026-04-14 14:18:07 +0800

    Revert "ROCm: replace hip_add_library/exec with native HIP"
    This reverts commit 3b2977f93d98c944e41592590bf4015d7609d2c8.

--- cmake/generic.cmake
+++ cmake/generic.cmake
@@ -782,13 +782,10 @@ function(hip_library TARGET_NAME)
     cmake_parse_arguments(hip_library "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN})
     if(hip_library_SRCS)
-      # Newer ROCm / CMake setups may not provide FindHIP.cmake macros like
-      # hip_add_library. Use CMake native HIP language support instead.
-      enable_language(HIP)
       if(hip_library_SHARED OR hip_library_shared) # build *.so
-        add_library(${TARGET_NAME} SHARED ${hip_library_SRCS})
+        hip_add_library(${TARGET_NAME} SHARED ${hip_library_SRCS})
       else()
-        add_library(${TARGET_NAME} STATIC ${hip_library_SRCS})
+        hip_add_library(${TARGET_NAME} STATIC ${hip_library_SRCS})
         find_fluid_modules(${TARGET_NAME})
         find_phi_modules(${TARGET_NAME})
       endif()
@@ -796,10 +793,11 @@ function(hip_library TARGET_NAME)
         add_dependencies(${TARGET_NAME} ${hip_library_DEPS})
         target_link_libraries(${TARGET_NAME} ${hip_library_DEPS})
       endif()
-      # Ensure .cu sources are compiled as HIP in ROCm builds.
+      # cpplint code style
       foreach(source_file ${hip_library_SRCS})
-        if(${source_file} MATCHES "\\.cu$")
-          set_source_files_properties(${source_file} PROPERTIES LANGUAGE HIP)
+        if(NOT ${source_file} MATCHES "\\.cu$")
+          set_source_files_properties(${source_file}
+                                      PROPERTIES HIP_SOURCE_PROPERTY_FORMAT 1)
         endif()
         string(REGEX REPLACE "\\.[^.]*$" "" source ${source_file})
         if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${source}.h)
@@ -830,13 +828,8 @@ function(hip_binary TARGET_NAME)
     set(multiValueArgs SRCS DEPS)
     cmake_parse_arguments(hip_binary "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN})
-    enable_language(HIP)
-    add_executable(${TARGET_NAME} ${hip_binary_SRCS})
-    foreach(source_file ${hip_binary_SRCS})
-      if(${source_file} MATCHES "\\.cu$")
-        set_source_files_properties(${source_file} PROPERTIES LANGUAGE HIP)
-      endif()
-    endforeach()
+    # FindHIP.cmake defined hip_add_executable, HIP_SOURCE_PROPERTY_FORMAT is required for .cc files
+    hip_add_executable(${TARGET_NAME} ${hip_binary_SRCS})
     if(hip_binary_DEPS)
       target_link_libraries(${TARGET_NAME} ${hip_binary_DEPS})
       add_dependencies(${TARGET_NAME} ${hip_binary_DEPS})
@@ -851,13 +844,9 @@ function(hip_test TARGET_NAME)
     set(multiValueArgs SRCS DEPS)
     cmake_parse_arguments(hip_test "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN})
-    enable_language(HIP)
-    add_executable(${TARGET_NAME} ${hip_test_SRCS})
-    foreach(source_file ${hip_test_SRCS})
-      if(${source_file} MATCHES "\\.cu$")
-        set_source_files_properties(${source_file} PROPERTIES LANGUAGE HIP)
-      endif()
-    endforeach()
+    # FindHIP.cmake defined hip_add_executable,
+    # HIP_SOURCE_PROPERTY_FORMAT is required for .cc files
+    hip_add_executable(${TARGET_NAME} ${hip_test_SRCS})
     # "-pthread -ldl -lrt" is defined in CMAKE_CXX_LINK_EXECUTABLE
     target_link_options(${TARGET_NAME} PRIVATE -pthread -ldl -lrt)
     get_property(os_dependency_modules GLOBAL PROPERTY OS_DEPENDENCY_MODULES)
